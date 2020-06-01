48e0176fa4dd7660b5f6cb4d0bec56957d2d9456
Jesús Guadalupe Macías Amador <jesus.macias.amador@gmail.com>
2020-05-31 22:12:50 -0500

    Agregando algunos iconos al sidebar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MaterialModule } from '../material/material.module';
+
 @NgModule({
   declarations: [
     HeaderComponent,
